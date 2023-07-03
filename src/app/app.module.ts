82d8a79ba3a799dc5c03ea3cb9ca5b31450f1df1
denissuhomlin <denis.suhomlin@b2b.discount>
2023-07-03 13:43:25 +0300

    feat(libs): add ngx-icon lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {B2bNgxIconModule} from "@b2b/ngx-icon";
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    B2bNgxIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
